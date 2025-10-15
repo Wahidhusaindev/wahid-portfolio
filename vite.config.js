b30805d5a700a3dc284624ba3940e2cdbf4b22cd
Wahid Husain <wahidhusaindev@gmail.com>
2025-10-15 08:53:25 +0000

    deployed on netlify

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/wahid-portfolio/",
-  plugins: [react(), tailwindcss()],
+   base: "/",
+  plugins: [react(), tailwindcss(), compression()],
 });
